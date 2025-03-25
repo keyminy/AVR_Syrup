cf71c9551976847c18eeab1dd44586bcb506ee53
keyminy <imanaifs@gmail.com>
2025-03-25 21:26:20 +0900

    ADC INT기반

--- ADC/ADC/main.c
+++ ADC/ADC/main.c
@@ -7,6 +7,14 @@
 
 FILE OUTPUT = FDEV_SETUP_STREAM(UART0_transmit,NULL, _FDEV_SETUP_WRITE);
 
+ISR(ADC_vect){
+	// ADSC가 0일때 ISR루틴 실행(conversion이 다 끝날때)
+	unsigned int Buf=(unsigned int)(ADCL | (ADCH<<8));
+	printf("%d\n",Buf);
+	_delay_ms(500);
+	//인터럽트가 끝날 때, 다시 ADSC를 1로 set해야함(변환을 다시 시작하겠다)
+	ADCSRA |= 1<<ADSC;
+}
 
 int main(void)
 {
@@ -22,17 +30,13 @@ int main(void)
 	// ADEN : ADC동작을 허용
 	// 단일모드 ADC변환을 한 번만 수행(ADFR = 0)
 	// ADC Prescaler Selections : 128분주
-	ADCSRA |= 1<<ADEN | 1 << ADPS0 | 1 << ADPS1 | 1<< ADPS2;
+	ADCSRA |= 1<<ADEN | 1<<ADIE
+	          | 1 << ADPS0 | 1 << ADPS1 | 1<< ADPS2;
+	sei(); // 전역적으로 인터럽트를 허용하겠다
 	
-	unsigned int Buf = 0; // AD변환할 데이터
-		sei(); // 전역적으로 인터럽트를 허용하겠다
+	// 처음에는 AD변환 하도록 신호 보내야됨.
+	ADCSRA |= 1<<ADSC;
     while (1) 
     {
-		ADCSRA |= 1 << ADSC; // Single Conversion의 경우 ADSC가 1이여야 변환 시작함.
-		while(ADCSRA & (1<<ADSC)); // ADCSRA의 ADSC는 변환이 끝나면 0으로 리턴됨
-		Buf = (unsigned int)(ADCL | (ADCH<<8));
-		printf("%d\n",Buf);
-		_delay_ms(1000);
     }
 }
-
